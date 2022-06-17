440e922264671eae94e1ae02bccb01f26bb03044
Hugo Averty <hugoaverty@gmail.com>
2022-06-17 14:39:55 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
             },
         },
     },
-    plugins: [],
+    plugins: [...getMapsOptimizers()],
     server: {
         host: "localhost",
         headers: {
